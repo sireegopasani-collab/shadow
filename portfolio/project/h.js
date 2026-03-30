a55db20cd875c9b37d45f26b13861ea9ff23561f
siree gopasani <sireegopasani@gmail.com>
2026-03-30 18:33:37 +0530

    ecommerce website

--- portfolio/project/h.js
+++
@@ -1,9 +0,0 @@
-<script>
-    let amount=prompt('Enter amount')
-    let password=prompt('Enter password')
-    if (name==password)
-    {
-        console.log('login successfully');
-        alert('login success');
-    }
-</script>
--- portfolio/project/yourskitchen/www.epicurious.com
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-<html>
-    <head>
-    <title>hi</title>
-    </head>
-    <body>
-        <h1>WELCOME TO Epicurious<h2>
-        </body>
-        </html>
